55bf6eff3e4c5028226e25bcbdcd79329948611a
Heorhii Siburov <g.siburov@gmail.com>
2019-09-13 09:45:49 -0700

    form is working now!!

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import MainCard from './MainCard'
+import SmurfForm from './smurfForm'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <h1>SMURFS! 2.0 W/ Redux</h1>
 
         <MainCard />
+        <SmurfForm />
       </div>
     );
   }
---
+++ smurfs/src/components/smurfForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+
+const SmurfForm = ({ errors, touched, status }) => {
+    const [smurfs, setSmurfs] = useState([]);
+  
+    useEffect(() => {
+        if (status) {
+            setSmurfs([...smurfs, status]);
+          }
+        }, [status, smurfs]);
+  
+    return (
+        <div>
+            <h3>Add a Smurf</h3>
+            <Form>
+                <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+        
+                <Field type="text" name="age" placeholder="Age" />
+                {touched.age && errors.age && <p className="error">{errors.age}</p>}
+
+                <Field type="text" name="height" placeholder="Height" />
+                {touched.height && errors.height && <p className="error">{errors.height}</p>}
+
+                <button type="submit">Submit!</button>
+            </Form>
+        </div>
+
+    );
+  };
+  
+  const FormikUserForm = withFormik({
+    mapPropsToValues({ name, age, height }) {
+      return {
+        name: name || '',
+        age: age || '',
+        height: height || '',
+      };
+    },
+  
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required('Please enter name'),
+      age: Yup.string().required('Please enter age'),
+      height: Yup.string().required('Please enter height'),
+    }),
+  
+    handleSubmit(values, { setStatus }) {
+      axios
+        .post('http://localhost:3333/smurfs', values)
+        .then(res => {
+          setStatus(values);
+          console.log(res);
+        })
+        .catch(err => console.log(err.response));
+    }
+  })(SmurfForm);
+  
+  export default FormikUserForm;
